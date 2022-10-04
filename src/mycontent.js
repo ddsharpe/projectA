223c511a493d0703f86789f0ed92bc1f8729724b
Derek Sharpe <derek.sharpe@oracle.com>
2022-10-04 14:56:50 -0500

    iterating

--- src/mycontent.js
+++ src/mycontent.js
@@ -10,7 +10,7 @@ if (project !== undefined) {
     const subfolder = project['subcomponents'].find(element => element['name'] === "projectA")
     if (subfolder !== undefined) {
         const image = subfolder['images'].find(element => element['image'] === "yyyy")
-        image['tag'] = "v2.3.5-derek"
+        image['tag'] = "v2.3.6-derek"
         console.log(image)
         foundTag = true;
     }
